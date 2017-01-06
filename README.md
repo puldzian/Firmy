43aa6bc13dac660e58d9841999c320e2c8d09534
Puldzian <puldzian@gmail.com>
2017-01-06 19:02:06 +0100

    dalej pod górę

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Firmy
 2017, indeks nazw przedsiębiorstw
 
-Baza firm pobrana skądinąd przy pomocy http://webscraper.io. Skrypcik R do połączenia, obróbki, porównania słowników (polski i angielski), a następnie wyrzucenie losowych próbek do pliku.
+Skrypcik R do połączenia, obróbki, porównania słowników (polski i angielski), a następnie wyrzucenie losowych próbek do pliku.
+
+Firmy: ściągnięte przy pomocy http://webscraper.io. 
+Nazwiska: http://www.futrega.org/etc/nazwiska.html
+Słowa polskie: http://sjp.pl/slownik/growy/
+Słowa angielskie: https://github.com/sujithps/Dictionary 
+Słowa niemieckie: 
--- firmy_main.R
+++ firmy_main.R
@@ -7,12 +7,13 @@
 # library(compare)
 library(dplyr)
 library(stringi)
+library(stringr)
 
 
 # DONE, jako tako
 importujBaze <- function() {
   # ładujemy pliczek z argumentu
-  baza <- read.csv(file = "bazafirm-test.csv", header = TRUE)
+  baza <- read.csv(file = "test/bazafirm-test.csv", header = TRUE)
   # ustaw alfabetycznie
   baza[,1] <- sort(baza[,1])
   # usuń dwie zbedne kolumny
@@ -34,16 +35,46 @@ importujBaze <- function() {
   colnames(baza) <- c("nazwafirmy")
   # wywal krótsze niż 3 znaki
   baza <- subset(baza, nchar(as.character(baza$nazwafirmy)) > 3)
+  # pokaż główkę
+  head(baza)
 }
 
 # DONE
 importujNazwiska <- function() {
-  nazwiska <- read.csv(file = "nazwiska-utf.txt", sep = " ", header = FALSE, flush = TRUE)
+  # wczytujemy pliczek nazwisk
+  nazwiska <- read.csv(file = "slowniki/nazwiska-utf.txt", sep = " ", header = FALSE, flush = TRUE)
+  # śmieci precz
   nazwiska[,1] <- NULL
+  # proszę o małe literki
   nazwiska <- as.data.frame(sapply(nazwiska,tolower))
+  # porządek w kolumnach
   colnames(nazwiska) <- c("nazwafirmy")
+  # kolejność alfabetyczna
   nazwiska[,1] <- sort(nazwiska[,1])
+  # wywal krótsze niż 3 znaki, bo nie używamy takich
+  nazwiska <- subset(nazwiska, nchar(as.character(nazwiska$nazwafirmy)) > 3)  
+  # pokaż główkę
+  head(nazwiska)
 }
+
+# 
+importujSlownikPL <- function() {
+  # wczytujemy pliczek nazwisk
+  slownikpl <- read.csv(file = "slowniki/slowa-utf.txt", sep = " ", header = FALSE, flush = TRUE)
+  # proszę o małe literki
+  slownikpl <- as.data.frame(sapply(slownikpl,tolower))
+  # porządek w kolumnach
+  colnames(slownikpl) <- c("nazwafirmy")
+  # kolejność alfabetyczna
+  slownikpl[,1] <- sort(slownikpl[,1])
+  # wypierdol te kretyńskie znaczki z końca linii
+  slownikpl$nazwafirmy <- stri_sub(slownikpl$nazwafirmy, 1, -3)
+  # wywal krótsze niż 3 znaki, bo nie używamy takich
+  slownikpl <- subset(slownikpl, nchar(as.character(slownikpl$nazwafirmy)) > 3)  
+  
+  # pokaż główkę
+  head(slownikpl)
+  }
 
 # to ma być maszyna do porównywania zbiorów
 porownajDane <- function(BAZA, SLOWNIK) {
